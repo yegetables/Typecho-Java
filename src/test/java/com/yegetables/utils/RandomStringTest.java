692f7844a776678cf7dc0e13c5cd12f3aea0ba6a
ajian2002 <ajian020515@163.com>
2022-04-04 09:55:44 +0800

    randomStringTest

---
+++ src/test/java/com/yegetables/utils/RandomStringTest.java
@@ -0,0 +1,9 @@
+package com.yegetables.utils;
+
+import org.junit.jupiter.api.Test;
+public class RandomStringTest  extends BaseJunit5Test {
+    @Test
+    void test(){
+        System.out.println(RandomTools.getRandomName());
+    }
+}
